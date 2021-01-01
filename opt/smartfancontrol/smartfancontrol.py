6e54d1bf61525c6b55ba3935e5680f70a0406bec
Helly <hellyrulez@home.nl>
2021-01-01 09:34:50 +0100

    v0.82

--- opt/smartfancontrol/smartfancontrol.py
+++ opt/smartfancontrol/smartfancontrol.py
@@ -34,7 +34,7 @@ from engine.tempctrl import tempctrl
 #########################################################
 
 ####################### GLOBALS #########################
-VERSION          = "0.81"
+VERSION          = "0.82"
 XML_FILENAME     = "smartfancontrol.xml"
 LOG_FILENAME     = "smartfancontrol.log"
 LOG_MAXSIZE      = 100*1024*1024
